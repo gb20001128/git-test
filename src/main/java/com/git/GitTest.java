d8596298ee10d45715be5d3236c418cfc2171f55
ph <ph@tt.com>
2022-02-24 20:12:56 +0800

    push commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
         System.out.println("Hello Git4!");
         System.out.println("master Test");
         System.out.println("hot-fix Test");
+        System.out.println("push Test");
 
     }
 }
