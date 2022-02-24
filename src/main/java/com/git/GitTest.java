774168e60fb8e8977a7f427f9a71cc2073b41b2c
gb20001128 <100279868+gb20001128@users.noreply.github.com>
2022-02-24 20:18:03 +0800

    pull commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("master Test");
         System.out.println("hot-fix Test");
         System.out.println("push Test");
+        System.out.println("pull Test");
 
     }
 }
