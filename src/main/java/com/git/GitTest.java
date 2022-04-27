63cd3a5fb1760059578164873a6d04e2771b31d8
gb20001128 <100279868+gb20001128@users.noreply.github.com>
2022-04-27 14:00:59 +0800

    Update GitTest.java

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -10,6 +10,40 @@ public class GitTest {
         System.out.println("hot-fix Test");
         System.out.println("push Test");
         System.out.println("pull Test");
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
+	<body>
+		<div id="timer" style="color: red"></div>
+	</body>
+	<script type="text/javascript">
+	var oBox=document.getElementById("timer");
+	var maxtime=6*60;
+
+	function CountDown()
+	{
+	 
+	 
+	 if(maxtime>-1)
+	 {
+	  minutes=Math.floor(maxtime/60);
+	  second=Math.floor(maxtime%60);
+	  msg="距离结束还有"+minutes+"分"+second+"秒";
+	  oBox.innerHTML=msg;
+	  if(maxtime==5*60) arert="还剩5分钟";
+	  --maxtime;
+	 }
+	 else
+	 {
+	   clearInterval(timer);
+	 }
+	}
+	timer = setInterval("CountDown()",1000);
+	</script>
+</html>
 
     }
 }
